d44ce8c721186bc08404b8ba243c608bf2585d5b
simple-evcorr <risto.vaarandi@gmail.com>
2020-11-11 18:45:54 +0200

    Update SecJson.pm

--- parsing-json/SecJson.pm
+++ parsing-json/SecJson.pm
@@ -94,7 +94,11 @@ sub json2matchvar {
   if (!defined($json_line)) { return; }
   if (!defined($prefix)) { $prefix = ""; }
 
-  $ptr = JSON::decode_json($json_line);
+  # mimic the default behavior of SEC input processing where Perl
+  # wide characters are not created from utf8 encoded characters
+  # (for enabling this behavior, JSON::decode_json() function)
+  
+  $ptr = JSON::from_json($json_line);
   $matchvar_hash = {};
 
   flatten($ptr, $matchvar_hash, $prefix); 
